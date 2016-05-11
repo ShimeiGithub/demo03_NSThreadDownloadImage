d7173f59a629d0153f3a2fd7192dd8990d935fae
LuoShimei <Luo_Shimei@163.com>
2016-05-11 15:25:10 +0800

    项目的创建
    使用线程下载图片

---
+++ demo03_NSThreadDownloadImage/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  demo03_NSThreadDownloadImage
+//
+//  Created by LuoShimei on 16/5/11.
+//  Copyright © 2016年 罗仕镁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+/** 点击下载图片按钮 */
+- (IBAction)clickDownloadImageButton:(id)sender {
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
